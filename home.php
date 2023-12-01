d23182b649dc70248026f48dae917f13704b7825
Cezar <maxchip@outlook.com>
2023-12-01 11:25:38 +0100

    Updates

--- home.php
+++ home.php
@@ -1,131 +1,97 @@
+<?php
+
+include_once 'assets/setup.php';
+
+$file_date = date("Y-m-d");
+
+// Json file name
+$sports_json = $_SERVER['DOCUMENT_ROOT'] . '/assets/json/' . $file_date . '/sports.json';
+
+$news_date = date('M d' . ', ' . 'Y');
+$news_category = 'sports';
+$news_item = '';
+
+// Check the file
+if (file_exists($sports_json)) {
+
+    if (is_file($sports_json) && is_readable($sports_json)) {
+
+        $data_json = json_decode(file_get_contents($sports_json), JSON_PRETTY_PRINT);
+        $news_item = $data_json['items'];
+
+        //var_dump($data_item);
+        //die();
+
+    } else {
+        header("Content-type: application/json");
+        echo json_encode("Error in sports.json file_get_contents()");
+    }
+} else {
+    header("Content-type: application/json");
+    echo json_encode("Error in sports.json file_exists()");
+    createNews(date("Y-m-d"));
+}
+
+?>
+
 <main class="pb-8 lg:pb-12 bg-white dark:bg-gray-900">
 
-    <div class="flex relative z-20 justify-center px-4 mx-auto max-w-screen-xl bg-white dark:bg-gray-900 rounded">
-        <div class="xl:w-[828px] mx-auto max-w-none dark:format-invert">
+        <div class="flex relative z-20 justify-center px-4 mx-auto max-w-screen-xl bg-white dark:bg-gray-900 rounded">
+            <div class="xl:w-[828px] mx-auto max-w-none dark:format-invert">
 
-            <!-- <section class="py-0">
-                <div class="container pt-3">
-                    <div class="flex flex-wrap -m-3 mb-16">
-                        <div class="w-full md:w-1/2 lg:w-1/3 p-3">
-                            <div class="p-6 border rounded-xl">
-                                <div class="relative h-52 mb-6">
-                                    <span class="absolute top-0 right-0 mt-4 mr-4 text-xs text-white px-2 py-1 font-semibold bg-gray-600 bg-opacity-50 rounded-md uppercase">Development</span>
-                                    <img class="w-full h-full object-cover rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/articles/image-1.png" alt="">
-                                </div>
-                                <span class="inline-block mb-2 text-xs text-gray-500">10 jun 2020 19:40</span>
-                                <h5 class="mb-2 font-semibold leading-tight text-gray-900 dark:text-white">Lorem ipsum dolor sit amet consectutar</h5>
-                                <a class="text-lg font-medium text-red-500 underline hover:no-underline" href="#">Read more</a>
-                            </div>
-                        </div>
-                        <div class="w-full md:w-1/2 lg:w-1/3 p-3">
-                            <div class="p-6 border rounded-xl">
-                                <div class="relative h-52 mb-6">
-                                    <span class="absolute top-0 right-0 mt-4 mr-4 text-xs text-white px-2 py-1 font-semibold bg-gray-600 bg-opacity-50 rounded-md uppercase">Development</span>
-                                    <img class="w-full h-full object-cover rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/articles/image-2.png" alt="">
-                                </div>
-                                <span class="inline-block mb-2 text-xs text-gray-500">10 jun 2020 19:40</span>
-                                <h5 class="mb-2 font-semibold leading-tight text-gray-900 dark:text-white">Lorem ipsum dolor sit amet consectutar</h5>
-                                <a class="text-lg font-medium text-red-500 underline hover:no-underline" href="#">Read more</a>
-                            </div>
-                        </div>
-                        <div class="w-full md:w-1/2 lg:w-1/3 p-3">
-                            <div class="p-6 border rounded-xl">
-                                <div class="relative h-52 mb-6">
-                                    <span class="absolute top-0 right-0 mt-4 mr-4 text-xs text-white px-2 py-1 font-semibold bg-gray-600 bg-opacity-50 rounded-md uppercase">Development</span>
-                                    <img class="w-full h-full object-cover rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/articles/image-3.png" alt="">
-                                </div>
-                                <span class="inline-block mb-2 text-xs text-gray-500">10 jun 2020 19:40</span>
-                                <h5 class="mb-2 font-semibold leading-tight text-gray-900 dark:text-white">Lorem ipsum dolor sit amet consectutar</h5>
-                                <a class="text-lg font-medium text-red-500 underline hover:no-underline" href="#">Read more</a>
-                            </div>
-                        </div>
+                <section class="py-0">
+                    <div class="container pt-5">
+                        <div class="flex flex-wrap -m-3 mb-16">
+                            
+                            <?php
 
-                        <div class="w-full md:w-1/2 lg:w-1/3 p-3">
-                            <div class="p-6 border rounded-xl">
-                                <div class="relative h-52 mb-6">
-                                    <span class="absolute top-0 right-0 mt-4 mr-4 text-xs text-white px-2 py-1 font-semibold bg-gray-600 bg-opacity-50 rounded-md uppercase">Development</span>
-                                    <img class="w-full h-full object-cover rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/articles/image-3.png" alt="">
-                                </div>
-                                <span class="inline-block mb-2 text-xs text-gray-500">10 jun 2020 19:40</span>
-                                <h5 class="mb-2 font-semibold leading-tight text-gray-900 dark:text-white">Lorem ipsum dolor sit amet consectutar</h5>
-                                <a class="text-lg font-medium text-red-500 underline hover:no-underline" href="#">Read more</a>
-                            </div>
-                        </div>
-                        <div class="w-full md:w-1/2 lg:w-1/3 p-3">
-                            <div class="p-6 border rounded-xl">
-                                <div class="relative h-52 mb-6">
-                                    <span class="absolute top-0 right-0 mt-4 mr-4 text-xs text-white px-2 py-1 font-semibold bg-gray-600 bg-opacity-50 rounded-md uppercase">Development</span>
-                                    <img class="w-full h-full object-cover rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/articles/image-2.png" alt="">
-                                </div>
-                                <span class="inline-block mb-2 text-xs text-gray-500">10 jun 2020 19:40</span>
-                                <h5 class="mb-2 font-semibold leading-tight text-gray-900 dark:text-white">Lorem ipsum dolor sit amet consectutar</h5>
-                                <a class="text-lg font-medium text-red-500 underline hover:no-underline" href="#">Read more</a>
-                            </div>
-                        </div>
-                        <div class="w-full md:w-1/2 lg:w-1/3 p-3">
-                            <div class="p-6 border rounded-xl">
-                                <div class="relative h-52 mb-6">
-                                    <span class="absolute top-0 right-0 mt-4 mr-4 text-xs text-white px-2 py-1 font-semibold bg-gray-600 bg-opacity-50 rounded-md uppercase">Development</span>
-                                    <img class="w-full h-full object-cover rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/articles/image-1.png" alt="">
-                                </div>
-                                <span class="inline-block mb-2 text-xs text-gray-500">10 jun 2020 19:40</span>
-                                <h5 class="mb-2 font-semibold leading-tight text-gray-900 dark:text-white">Lorem ipsum dolor sit amet consectutar</h5>
-                                <a class="text-lg font-medium text-red-500 underline hover:no-underline" href="#">Read more</a>
-                            </div>
-                        </div>
+                                if(count($news_item) > 0) {
 
-                        <div class="w-full md:w-1/2 lg:w-1/3 p-3">
-                            <div class="p-6 border rounded-xl">
-                                <div class="relative h-52 mb-6">
-                                    <span class="absolute top-0 right-0 mt-4 mr-4 text-xs text-white px-2 py-1 font-semibold bg-gray-600 bg-opacity-50 rounded-md uppercase">Development</span>
-                                    <img class="w-full h-full object-cover rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/articles/image-1.png" alt="">
-                                </div>
-                                <span class="inline-block mb-2 text-xs text-gray-500">10 jun 2020 19:40</span>
-                                <h5 class="mb-2 font-semibold leading-tight text-gray-900 dark:text-white">Lorem ipsum dolor sit amet consectutar</h5>
-                                <a class="text-lg font-medium text-red-500 underline hover:no-underline" href="#">Read more</a>
-                            </div>
-                        </div>
-                        <div class="w-full md:w-1/2 lg:w-1/3 p-3">
-                            <div class="p-6 border rounded-xl">
-                                <div class="relative h-52 mb-6">
-                                    <span class="absolute top-0 right-0 mt-4 mr-4 text-xs text-white px-2 py-1 font-semibold bg-gray-600 bg-opacity-50 rounded-md uppercase">Development</span>
-                                    <img class="w-full h-full object-cover rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/articles/image-2.png" alt="">
-                                </div>
-                                <span class="inline-block mb-2 text-xs text-gray-500">10 jun 2020 19:40</span>
-                                <h5 class="mb-2 font-semibold leading-tight text-gray-900 dark:text-white">Lorem ipsum dolor sit amet consectutar</h5>
-                                <a class="text-lg font-medium text-red-500 underline hover:no-underline" href="#">Read more</a>
-                            </div>
-                        </div>
-                        <div class="md:w-1/2 lg:w-1/3 p-3">
-                            <div class="p-6 border rounded-xl">
-                                <div class="relative h-52 mb-6">
-                                    <span class="absolute top-0 right-0 mt-4 mr-4 text-xs text-white px-2 py-1 font-semibold bg-gray-600 bg-opacity-50 rounded-md uppercase">Development</span>
-                                    <img class="w-full h-full object-cover rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/articles/image-3.png" alt="">
-                                </div>
-                                <span class="inline-block mb-2 text-xs text-gray-500">10 jun 2020 19:40</span>
-                                <h5 class="mb-2 font-semibold leading-tight text-gray-900 dark:text-white">Lorem ipsum dolor sit amet consectutar</h5>
-                                <a class="text-lg font-medium text-red-500 underline hover:no-underline" href="#">Read more</a>
-                            </div>
+                                    foreach ($news_item as $key => $item) { 
+                                        
+                                        echo '<div class="flex items-stretch w-full md:w-1/2 lg:w-1/3 p-3">';
+                                        echo '<div class="p-6 rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">';
+                                        echo '<div class="relative h-52 mb-6">';
+                                        echo '<span class="absolute top-0 right-0 mt-4 mr-4 text-xs text-white px-2 py-1 font-semibold bg-gray-600 bg-opacity-50 rounded-md uppercase">' . $news_category . '</span>';
+                                        
+                                        if (array_key_exists('image', $item)) {
+                                            echo '<img class="w-full h-full object-cover rounded-lg" src="' . $item['image'] . '" alt="'. $news_category . '_image_' . $key . '">';
+                                        } else {
+                                            echo '<img class="w-full h-full object-cover rounded-lg" src="assets/logos/logo-180x180.png" alt="'. $news_category . '_image_' . $key . '">';
+                                        }
+                                       
+                                        echo '</div>';
+                                        echo '<span class="inline-block mb-2 text-xs text-gray-500 dark:text-gray-400">' . $news_date . '</span>';
+                                        echo '<h5 class="mb-2 font-semibold leading-tight text-gray-900 dark:text-white">'. $item['title'] . '</h5>';
+                                        echo '<p class="mb-2 font-light text-gray-500 dark:text-gray-400">'. $item['content_text'] . '</p>';
+                                        echo '<a href="'. $item['url'] . '" class="inline-flex items-center font-medium text-primary-600 hover:underline dark:text-primary-500" target="_blank">';
+                                        echo 'Read more';
+                                        echo '<svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                                             </svg>';
+                                        echo '</a>';
+                                        echo '</div>';
+                                        echo '</div>';
+                                    }
+                                }
+                            ?>
                         </div>
                     </div>
-                    <div class="text-center"><a class="px-6 py-4 text-sm text-white font-semibold bg-red-400 hover:bg-red-300 rounded transition duration-200" href="#">View More Articles</a></div>
-                </div>
-            </section> -->
+                </section>
 
-            <rssapp-wall id="tP8kWRI6p7bKNb5M">
-            </rssapp-wall>
-            <script src="https://widget.rss.app/v1/wall.js" type="text/javascript" async>
-            </script>
-
-        </div>
-        <aside class="hidden xl:block" aria-labelledby="sidebar-label">
-            <div class="xl:w-[336px] sticky pt-5 top-2">
-                <?php
-                include_once 'newscalendar.php';
-                include_once 'rightsidetodaynews.php';
-                include_once 'newsletterbox.php';
-                ?>
             </div>
-        </aside>
-    </div>
-</main>
+            <aside class="hidden xl:block" aria-labelledby="sidebar-label">
+                <div class="xl:w-[336px] sticky pt-5 top-2">
+                    <?php
+                    include_once 'newscalendar.php';
+                    include_once 'rightsidetodaynews.php';
+                    include_once 'newsletterbox.php';
+                    ?>
+                </div>
+            </aside>
+        </div>
+    </main>
+    
+    <!-- SCRIPTS -->
+    
+</body>
--- science.php
+++ science.php
@@ -1,5 +1,37 @@
 <?php
 
+include_once 'assets/setup.php';
+
+$file_date = date("Y-m-d");
+
+// Json file name
+$science_json = $_SERVER['DOCUMENT_ROOT'] . '/assets/json/' . $file_date . '/science.json';
+
+$news_date = date('M d' . ', ' . 'Y');
+$news_category = 'science';
+$news_item = '';
+
+// Check the file
+if (file_exists($science_json)) {
+
+    if (is_file($science_json) && is_readable($science_json)) {
+
+        $data_json = json_decode(file_get_contents($science_json), JSON_PRETTY_PRINT);
+        $news_item = $data_json['items'];
+
+        //var_dump($data_item);
+        //die();
+
+    } else {
+        header("Content-type: application/json");
+        echo json_encode("Error in science.json file_get_contents()");
+    }
+} else {
+    header("Content-type: application/json");
+    echo json_encode("Error in science.json file_exists()");
+    createNews(date("Y-m-d"));
+}
+
 $pageTitle = 'Science - Get Hit News';
 
 // Google
@@ -41,13 +73,51 @@ include_once 'head.php';
         <div class="flex relative z-20 justify-center px-4 mx-auto max-w-screen-xl bg-white dark:bg-gray-900 rounded">
             <div class="xl:w-[828px] mx-auto max-w-none dark:format-invert">
 
-                <rssapp-wall id="trz8SbQ6vmUNr0l8"></rssapp-wall>
-                <script src="https://widget.rss.app/v1/wall.js" type="text/javascript" async></script>
+                <section class="py-0">
+                    <div class="container pt-5">
+                        <div class="flex flex-wrap -m-3 mb-16">
+                            
+                            <?php
+
+                                if(count($news_item) > 0) {
+
+                                    foreach ($news_item as $key => $item) { 
+                                        
+                                        echo '<div class="flex items-stretch w-full md:w-1/2 lg:w-1/3 p-3">';
+                                        echo '<div class="p-6 rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">';
+                                        echo '<div class="relative h-52 mb-6">';
+                                        echo '<span class="absolute top-0 right-0 mt-4 mr-4 text-xs text-white px-2 py-1 font-semibold bg-gray-600 bg-opacity-50 rounded-md uppercase">' . $news_category . '</span>';
+                                        
+                                        if (array_key_exists('image', $item)) {
+                                            echo '<img class="w-full h-full object-cover rounded-lg" src="' . $item['image'] . '" alt="'. $news_category . '_image_' . $key . '">';
+                                        } else {
+                                            echo '<img class="w-full h-full object-cover rounded-lg" src="assets/logos/logo-180x180.png" alt="'. $news_category . '_image_' . $key . '">';
+                                        }
+                                       
+                                        echo '</div>';
+                                        echo '<span class="inline-block mb-2 text-xs text-gray-500 dark:text-gray-400">' . $news_date . '</span>';
+                                        echo '<h5 class="mb-2 font-semibold leading-tight text-gray-900 dark:text-white">'. $item['title'] . '</h5>';
+                                        echo '<p class="mb-2 font-light text-gray-500 dark:text-gray-400">'. $item['content_text'] . '</p>';
+                                        echo '<a href="'. $item['url'] . '" class="inline-flex items-center font-medium text-primary-600 hover:underline dark:text-primary-500" target="_blank">';
+                                        echo 'Read more';
+                                        echo '<svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                                             </svg>';
+                                        echo '</a>';
+                                        echo '</div>';
+                                        echo '</div>';
+                                    }
+                                }
+                            ?>
+                        </div>
+                    </div>
+                </section>
 
             </div>
             <aside class="hidden xl:block" aria-labelledby="sidebar-label">
                 <div class="xl:w-[336px] sticky pt-5 top-2">
                     <?php
+                    include_once 'newscalendar.php';
                     include_once 'rightsidetodaynews.php';
                     include_once 'newsletterbox.php';
                     ?>
@@ -64,7 +134,6 @@ include_once 'head.php';
 
     <script src="assets/js/flowbite181.js"></script>
     <script src="assets/js/dark-mode.js"></script>
+    <script src="assets/js/datepicker.js"></script>
 
 </body>
-
-</html>
